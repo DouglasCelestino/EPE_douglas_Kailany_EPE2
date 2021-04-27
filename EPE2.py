4295e7372d53fb22fdfb1eee7a434e3974f4c580
Douglas <douglaspablo2018@gmail.com>
2021-04-27 20:46:43 -0300

    função lista de movimentos possíveis

--- EPE2.py
+++ EPE2.py
@@ -25,3 +25,35 @@ def extrai_valor (carta):
     if len(carta) == 3:
         return carta[0]+ carta[1]
     return carta[0]
+
+def lista_movimentos_possiveis(baralho,ind):
+    naipe_carta = extrai_naipe(baralho[ind])
+    naipe_anterior = extrai_naipe(baralho[ind-1])
+    valor_carta = extrai_valor(baralho[ind])
+    valor_anterior = extrai_valor(baralho[ind-1])
+    if ind == 0:  
+        return []
+    elif ind < 3:
+        if naipe_carta == naipe_anterior or valor_carta == valor_anterior:
+            return [1]
+        else:
+            return []
+    elif ind >= 3:
+        naipe_terceiro = extrai_naipe(baralho[ind-3])
+        valor_terceiro = extrai_valor(baralho[ind-3])
+        if valor_carta == valor_anterior and valor_carta == valor_terceiro:
+            return [1, 3]
+        elif naipe_carta == naipe_anterior and naipe_carta == naipe_terceiro:
+            return [1, 3]
+        elif valor_carta == valor_anterior and naipe_carta == naipe_terceiro:
+            return [1, 3]
+        elif naipe_carta == naipe_anterior and valor_carta == valor_terceiro:
+            return [1, 3]
+        elif naipe_carta == naipe_anterior or valor_carta == valor_anterior:
+            return [1]
+        elif valor_carta == valor_terceiro or naipe_carta == naipe_terceiro:
+            return [3]
+        else:
+            return []
+    else:
+        return []
