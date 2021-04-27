4d0cb734dbb2062daa9ed981fff4b30302f0b8f5
Douglas <douglaspablo2018@gmail.com>
2021-04-27 20:15:33 -0300

    Função extrai naipe

--- EPE2.py
+++ EPE2.py
@@ -14,3 +14,9 @@ def cria_baralho():
         Baralho.append(juntar3)
         Baralho.append(juntar4)
     return Baralho
+
+def extrai_naipe(carta):
+    if carta[0] == '1':
+        return(carta[2])
+    else:
+        return(carta[1])
